e390aae471fa6e22698797e98c86c999049580f7
Johann Steinbrecher <johann.steinbrecher@JSteinbrecher-MBP.local>
2016-07-28 17:29:38 -0700

    Secure webrtc signaling
    - [x] Proof of concept

---
+++ README.md
@@ -0,0 +1,2 @@
+# Secure webrtc signaling - proof of concept
+[Architecture diagram](https://docs.google.com/presentation/d/1kTuesyISq-X03Mcx62v2_HD5o7-Q-NUnHWAxYHhNqq8/edit#slide=id.g15fbd73bca_0_0)
---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    redis   = require('redis');
+
+var PORT = 8080;
+var db   = redis.createClient();
+
+var app = express();
+var key = 'rtc.cookie.';
+
+// Enforce user permissions
+app.get('/validate', function (req, res) {
+
+    console.log('Validate cookie', req.headers.cookie);
+
+    db.get(key+ req.headers.cookie, function(err, value) {
+        if (value) {
+            res.sendStatus(204);
+        } else {
+            res.sendStatus(401);
+        }
+    });
+
+});
+
+// Grant user permissions
+app.get('/grant', function (req, res) {
+
+    console.log('Grant cookie', req.headers.cookie);
+
+    db.set(key + req.headers.cookie, true, function(err, value) {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(204);
+        }
+    });
+
+});
+
+app.get('/jitsi', function (req, res) {
+    console.log('signaling starts');
+    res.send('jitsi');
+});
+
+app.listen(PORT, function () {
+    console.log('Listening on port ' + PORT);
+});
