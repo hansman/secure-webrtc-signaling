b01c33d1ce415639e0e4920ac22ba3135a9f6851
Johann Steinbrecher <johann.steinbrecher@symphony.com>
2017-01-12 14:07:56 -0800

    Public/private keys as arguments.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Generate a token
+
+`node generateJWT.js -r testroom -u symphony.com.public.openssl -v symphony.com.openssl`
+
 # Secure webrtc signaling - proof of concept
 [Architecture](https://docs.google.com/presentation/d/1kTuesyISq-X03Mcx62v2_HD5o7-Q-NUnHWAxYHhNqq8/edit#slide=id.g15fbd73bca_0_0)
 
@@ -45,9 +49,3 @@ Dependencies: nginx, node, redis, [ngx_http_auth_request_module](http://nginx.or
 In `/etc/hosts` set `54.197.39.123 rtc.symphony.com`
 
 Load in your browser `https://rtc.symphony.com/{roomID}#config.token="{token}"`
-
-
-
-
-
-
--- generateJWT.js
+++ generateJWT.js
@@ -3,24 +3,41 @@
 var jwt     = require('jsonwebtoken'),
     fs      = require('fs'),
     argv    = require('yargs')
-    .usage('Usage: $0 -r [string] -s [string] -i [string] -e [num]')
+    .usage('Usage: $0 -r [string] -s [string] -v [string] -u [string] -i [string] -e [num] -p [num]')
     .alias('r', 'room')
     .alias('s', 'appSecret')
     .alias('i', 'appID')
     .alias('e', 'expires')
-    .demand(['r'])
+    .alias('v', 'private')
+    .alias('u', 'public')
+    .demand(['r', 'u', 'v'])
     .argv;
 
-var appID = argv.i || 'rtc-signaling-dev';
+var appID = argv.i || 'rtc-signaling-uat';
+var podID = argv.p || 1;
 var appSecret = argv.s || 'example_app_secret';
 var room = argv.r.toLowerCase();
 var expire = argv.e || 5;
 
 var claims = {
     iss: appID,
+    podId: podID,
+    streamId: 'exampleStreamId',
+    userId: 71811853189570,
     room: room,
-    exp: Date.now() + 24 * 60 * 365 * 60000 * 5
+    exp: Date.now() + 24 * 60 * 365 * 60000 * 5,
+    wildcard: true
 };
-var cert = fs.readFileSync('rtc.pkcs8');
-console.log(jwt.sign(claims,  cert, { algorithm: 'RS512'}));
-process.exit(0);
+
+const token = jwt.sign(claims,  fs.readFileSync(argv.v), { algorithm: 'RS512'});
+
+console.log('token', token);
+
+jwt.verify(token, fs.readFileSync(argv.u), {}, function(err, payload) {
+    if (err) {
+        console.warn('invalid', err);
+        process.exit(1);
+    }
+    console.info('valid', payload);
+    process.exit(0);
+});
